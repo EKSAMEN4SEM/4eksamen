5d8f4cee8362adb1778a836aa785ba08f13dd2e0
Rasmus <rame0001@stud.kea.dk>
2025-12-05 22:54:01 +0100

    Refactor SingleProjekt component; enhance layout and improve text rendering

--- src/app/projekter/[id]/page.jsx
+++ src/app/projekter/[id]/page.jsx
@@ -1,19 +1,29 @@
 import { getDataArtikel } from "@/src/lib/fetching";
-import Glasscard from '../../components/glasscard'
-
 
 export default async function SingleProjekt(props) {
     const params = await props.params;
     const projektData = await getDataArtikel(params.id);
     const data = projektData[0];
 
-    return (
-        <section className="w-(--content-size) py-(--content-padding) m-auto">
-            <div className="glass-card p-12 flex flex-col items-center">
-                <img className="object-cover h-[500px] w-full rounded-lg" src={data.img} alt="Sektions billede" />
-                <h1 className="text-4xl font-(--font-weight) mt-8">{data.overskrift}</h1>
-                <p className="text-lg mt-4">{data.broedtekst}</p>
+ return (
+    <section className="w-(--content-size) py-(--content-padding) m-auto [&>div]:p-18">
+        <div className="glass-card flex flex-col items-center">
+            <img
+                className="object-cover h-[500px] w-full rounded-lg"
+                src={data.img}
+                alt="Sektions billede"
+            />
+            <h1 className="text-6xl text-center font-(--font-weight) mt-12">
+                {data.overskrift}
+            </h1>
+            <div className="my-12">
+            {data.broedtekst.map((afsnit, index) => (
+                <p key={index} className="text-lg mt-6 font-medium">
+                    {afsnit}
+                </p>
+            ))}
             </div>
-        </section>
-    )
+        </div>
+    </section>
+);
 }
