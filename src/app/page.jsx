de80d41f3f31aa4f87ca540f7a7908910ed3df59
Rasmus <rame0001@stud.kea.dk>
2025-12-08 11:35:04 +0100

    Fix formatting of Katalog component tag for consistency

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default async function Home() {
   <>
   <Branchecard />
   < Cementsektion />
-  <Katalog />
+  < Katalog />
   <ProjektCard />
   < Cementtal />
 {/*     <LearningThree /> */}
