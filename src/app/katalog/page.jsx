22bc235c07a165b6d6148733a0ba8ce8ea6f7a1f
Jesper <jefy0001@stud.kea.dk>
2025-12-09 14:30:21 +0100

    Refactor Katalog component; improve structure and add Herosection for enhanced layout

--- src/app/katalog/page.jsx
+++ src/app/katalog/page.jsx
@@ -1,29 +1,34 @@
 import { getDataAlleProdukter } from "@/src/lib/fetching";
 import Glasscard from "../components/glasscard";
-import Sektionsoverskrift from "../components/sektionsoverskrift"
+import Sektionsoverskrift from "../components/sektionsoverskrift";
+import Herosection from "../sektioner/herosection";
 import Link from "next/link";
 
 export default async function Katalog() {
-
-    const produkterData = await getDataAlleProdukter();
+  const produkterData = await getDataAlleProdukter();
 
   return (
     <>
-    <section className="bg-[url(/jordfarvet-bg.png)] bg-cover bg-center w-full h-fit items-center py-(--content-padding)">
-    < Sektionsoverskrift titel="Katalog"/>
-      {produkterData.map((p) => (
-    <Link key={p.id} href={`/katalog/${p.id}`}>
-        <Glasscard
-          key={p.id}
-          overskrift={p.overskrift}
-          img={p.hovedimg}
-          tekst={p.korttekst}
-        />
-      </Link>
-      ))
-      }
+      <Herosection
+        bgUrl="/katalog_hero.png"
+        height="h-[60vh]"
+      />
+      <section className="bg-[url(/jordfarvet-bg.png)] bg-cover bg-center w-full h-fit items-center py-(--content-padding)">
+        <Sektionsoverskrift titel="Katalog" />
+        {produkterData.map((p) => (
+          <Link
+            key={p.id}
+            href={`/katalog/${p.id}`}
+          >
+            <Glasscard
+              key={p.id}
+              overskrift={p.overskrift}
+              img={p.hovedimg}
+              tekst={p.korttekst}
+            />
+          </Link>
+        ))}
       </section>
     </>
-    
   );
 }
