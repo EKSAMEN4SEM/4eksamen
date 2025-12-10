e63cd7f2e45bb59c1b1bdd4a323f382a66720277
Jesper <jefy0001@stud.kea.dk>
2025-12-10 11:26:12 +0100

    Add Herosection component to Omos page for enhanced layout

--- src/app/omos/page.jsx
+++ src/app/omos/page.jsx
@@ -1,11 +1,16 @@
-import Maalsaetning from "../components/maalsaetning"
-import Profilkort from "../components/profilkort"
+import Maalsaetning from "../components/maalsaetning";
+import Profilkort from "../components/profilkort";
+import Herosection from "../sektioner/herosection";
 
-export default function Omos () {
-    return (
-        <>
-            < Maalsaetning />
-            < Profilkort />
-        </>
-    )
+export default function Omos() {
+  return (
+    <>
+      <Herosection
+        bgUrl="/omos_hero.png"
+        height="h-[60vh]"
+      />
+      <Maalsaetning />
+      <Profilkort />
+    </>
+  );
 }
